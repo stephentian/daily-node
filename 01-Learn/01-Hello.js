e0155ae2ac7403cad220ac1bf2a5aa856139be6a
Stephen Tian <745752067@qq.com>
2018-10-24 16:52:19 +0800

    更新文件夹，上传资源

---
+++ 01-Learn/01-Hello.js
@@ -0,0 +1,18 @@
+// Load HTTP module
+var http = require('http');
+
+// Create HTTP server and listen on port 8000 for requests
+http.createServer(function (request, response) {
+
+  // Set the response HTTP header with HTTP status and Content type
+  response.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+
+  // Send the response body "Hello World"
+  response.end('Hello World\n');
+}).listen(8000);
+
+// Print URL for accessing server
+
+console.log('Server running at http://10.0.203.83:8080');
