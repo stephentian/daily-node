5d50c9c31070dad80f3a70b148a6833c2f3bcd83
Stephen Tian <745752067@qq.com>
2019-01-02 20:58:04 +0800

    node server

--- 02-Express/01-Hello.js
+++ 02-Express/01-Hello.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! Express');
 });
 
 app.listen(3000, function () {
-  console.log('Express listen on port 8000!');
+  console.log('Express listen on port 3000!');
 })
