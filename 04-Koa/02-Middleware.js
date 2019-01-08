ce70bed7dd0fb52acdc0211ab0391b5a9ec6f485
Stephen Tian <745752067@qq.com>
2019-01-08 20:42:40 +0800

    koa middleware

---
+++ 04-Koa/02-Middleware.js
@@ -0,0 +1,46 @@
+/**
+ * author: stephentian
+ * email: stephentian@foxmail.com
+ * day: 2019-1-8
+ **/
+
+// koa middleware
+
+// 先看看 koa 执行逻辑
+const Koa = require('koa')
+
+const app = new Koa()
+
+// app.use(async (ctx, next) => {
+//   await next()
+//   ctx.response.type = 'text/html'
+//   ctx.response.body = '<h1>Hello Koa Middleware !</h1>'
+// })
+
+// 每收到一个 http 请求， koa 就会调用 app.use() 注册 async 函数，并传入 ctx 和 next 参数
+// 为什么要调用 await next() 呢？
+// 原因是 koa 把很多 async 函数组成处理链，每个 async 都可以做自己的事情，然后 await next() 来调用下一个 async 函数
+// 把每个 async 函数称为 middleware
+// 这些 middleware 组合起来，可以完成很多功能
+
+// 例如，用一下 3 个 middleware 组成处理链，依次打印日志，记录处理时间，输出 HTML
+app.use(async (ctx, next) => {
+  console.log(`${ctx.request.method} ${ctx.request.url}`) // 打印 url
+  await next() // 调用下一个 middleware
+})
+
+app.use(async (ctx, next) => {
+  const start = new Date().getTime() // 当前时间
+  await next()
+  const ms = new Date().getTime() - start // 耗费时间
+  console.log(`Time: ${ms}ms`)
+})
+
+app.use(async (ctx, next) => {
+  await next()
+  ctx.response.type = 'text/html'
+  ctx.response.body = '<h1>Hello Koa Middleware !</h1>'
+})
+
+app.listen(3000)
+console.log('Server runnig at port 3000 !')
--- 04-Koa/Server.js
+++ 04-Koa/Server.js
@@ -5,15 +5,20 @@
  **/
 
 // Koa
-const koa = require('koa')
-const app = new koa()
+const Koa = require('koa')
+const app = new Koa()
+
 
 app.use(async (ctx, next) => {
   await next()
-  const data = await doReadFile()
-  ctx.response.type = 'text/plain'
-  ctx.response.body = data
+  ctx.response.type = 'text/html'
+  ctx.response.body = '<h1>Hell Koa2 !</h1>'
 })
 
 app.listen(3000)
-console.log('Server runnig at host 3000 !')
+console.log('Server runnig at port 3000 !')
+
+// 对于每个 http 请求， koa 将调用我们传入的异步函数 next 处理
+// 参数 ctx 是由 koa 传入的封装了 request 和 response 的变量
+// next 是 koa 传入的将要处理的下一个异步函数
+// 由 async 标记的函数称为 异步函数； 在异步函数中，可以用 await 调用另一个异步函数
