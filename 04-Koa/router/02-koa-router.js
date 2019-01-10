2cca41e485ae9707b218d746bd0d249026c03eb1
Stephen Tian <745752067@qq.com>
2019-01-10 23:58:06 +0800

    koa-router

---
+++ 04-Koa/router/02-koa-router.js
@@ -0,0 +1,13 @@
+/**
+ * author: stephentian
+ * email: stephentian@foxmail.com
+ * day: 2019/1/10
+ **/
+
+// koa-router
+const Koa = require("koa")
+
+// 注意 require koa-router 返回的函数
+const router = require("koa-router")()
+
+const app = new Koa()
