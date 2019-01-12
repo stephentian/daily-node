8d7632489090d33ef328d8e632e065a1862fac1f
Stephen Tian <745752067@qq.com>
2019-01-12 10:36:23 +0800

    koa-router get

--- 04-Koa/router/01-url.js
+++ 04-Koa/router/01-url.js
@@ -10,31 +10,31 @@
 const Koa = require('koa')
 const app = new Koa()
 
-// app.use(async (ctx, next) => {
-//   if (ctx.request.url === '/') {
-//     ctx.response.body = 'this is index page!'
-//   } else {
-//     await next()
-//   }
-// })
+app.use(async (ctx, next) => {
+  if (ctx.request.url === '/') {
+    ctx.response.body = 'this is index page!'
+  } else {
+    await next()
+  }
+})
 
-// app.use(async (ctx, next) => {
-//   if (ctx.request.url === '/test') {
-//     ctx.response.body = 'Test page!'
-//   } else {
-//     await next()
-//   }
-// })
+app.use(async (ctx, next) => {
+  if (ctx.request.url === '/test') {
+    ctx.response.body = 'Test page!'
+  } else {
+    await next()
+  }
+})
 
-// app.use(async (ctx, next) => {
-//   if (ctx.request.url === '/error') {
-//     ctx.response.body = 'ERROR page!'
-//   } else {
-//     await next()
-//   }
-// })
+app.use(async (ctx, next) => {
+  if (ctx.request.url === '/error') {
+    ctx.response.body = 'ERROR page!'
+  } else {
+    await next()
+  }
+})
 
-// app.listen(3001)
+app.listen(3001)
 
 // 这么写是可以运行的，但是好像有点蠢，有几十上百个路由怎么办？
 // 应该有个能集中处理 URL 的 middleware, 它能根据不同的 URL 调用不同的处理函数，这样，我们才能专心为每个 URL 编写处理函数
--- 04-Koa/router/02-koa-router.js
+++ 04-Koa/router/02-koa-router.js
@@ -9,11 +9,31 @@ const Koa = require("koa")
 
 // 注意 require koa-router 返回的函数
 const router = require("koa-router")()
+// 注意导入 koa-ruter 的语句最后是函数调用
+// 相当于:
+// const fn_router = require('koa-router')
+// const router = fn_router()
 
 const app = new Koa()
+
 
 // log request URL
 app.use(async (ctx, next) => {
   console.log(`Process ${ctx.request.methods} ${ctx.request.url}...`)
   await next()
 })
+
+router.get('/hello/:name', async (ctx, next) => {
+  const name = ctx.params.name
+  ctx.response.body = `<h1>Hello, ${name}</h1>`
+})
+
+router.get('/', async (ctx, next) => {
+  ctx.response.body = `<h1>这里是首页！</h1>`
+})
+
+app.use(router.routes())
+
+app.listen(3000)
+
+console.log('app started at port 3000...')
