f505fb2543723c5397ee1ef17143401567eacc0a
Stephen Tian <745752067@qq.com>
2019-02-02 18:05:53 +0800

    view

--- 05-MongoDB/todo/todo/src/store/index.js
+++ 05-MongoDB/todo/todo/src/store/index.js
@@ -37,6 +37,7 @@ export default new Vuex.Store({
     msg: 'Todo App',
     todos: defaultTodo
   },
+  // mutations 提供方法
   mutations: {
     add(state, subject) {
       var todo = {
@@ -52,6 +53,10 @@ export default new Vuex.Store({
       state.todos = defaultTodo
     }
   },
+  // 为了避免阻塞客户端主线程，提供的异步方法
+  // actions 对象内的方法和功能和 mutations 一致, 但是是异步的
+  // context.commit() 语法，提供 actions 和 mutations 方法的对接
+  // 第一个参数是 mutations 函数名, 之后的参数作为 前面函数名的参数 传给 mutations 方法
   actions: {
     add: (context, link) => {
       context.commit("add", link)
---
+++ 05-MongoDB/todo/todo/src/views/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  computed: mapState(['todos', 'msg'])
+}
+</script>
+
+<style scoped>
+</style>
