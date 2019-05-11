c62b8392301bedbc33dd167fb8314a2675249a83
Stephen Tian <745752067@qq.com>
2019-05-11 22:24:51 +0800

    module

---
+++ 01-Learn/modules/index.js
@@ -0,0 +1,14 @@
+/**
+* author: stephentian
+* email: stephentian@foxmail.com
+* day: 2019-5-11
+**/
+
+// 模块实现历程
+
+// 1. 闭包， 自执行函数(jQuery)
+// 2. require.js AMD 规范
+// 3. sea.js CMD 
+// 4. node.js common.js
+// 5. es6 es_module
+// 6. umd (AMD + CMD + ES + module)
